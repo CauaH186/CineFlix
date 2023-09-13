1765bdad65d9a550958f35502c1fb1eeb714c298
CauaH186 <caua186@gmail.com>
2023-09-13 19:28:35 -0300

    Adicionando a Pasta SRC

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+const API_URL = "http://www.omdbapi.com?apikey=af7c1d34";
+
+function App() {
+  const [searchTerm, setSearchTerme] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>CineFlix</h1>
+
+      <div className="search">
+        <input
+          placeholder="Porcure por filmes"
+          value={searchTerm}
+          onChange={(e) => setSearchTerme(e.target.value)}
+          onKeyUp={() => searchMovies(searchTerm)}
+        />
+        <img
+          src={SearchIcon}
+          alt="Buscar"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>Nenhum filme foi encontrado</h2>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
